be3ed3a9c37178e206c844ba571e444c3a4bc1c5
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-10-27 17:49:56 +0100

    As an Admin I can Create User

--- src/pages/Admin/Restaurants.jsx
+++ src/pages/Admin/Restaurants.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { StatisticsTable } from "../../components/Dashboard/StatisticsTable";
-import { fetchAllRestaurants, acceptRestaurant } from "../../services/RestaurantsService";
+import {
+  fetchAllRestaurants,
+  acceptRestaurant,
+} from "../../services/RestaurantsService";
 import toast, { Toaster } from "react-hot-toast";
 import { PropagateLoader } from "react-spinners";
+import { CreateUserForm } from "../../components/Dashboard/CreateUserModal";
 
 export const Restaurants = () => {
   const tableHeader = [
@@ -14,6 +18,8 @@ export const Restaurants = () => {
 
   const [tableData, setTableData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [restaurantCreation, setRestaurantCreation] = useState(1);
 
   const notify = ({ message, type = "info", duration }) => {
     toast[type](message, {
@@ -100,7 +106,6 @@ export const Restaurants = () => {
       setLoading(false);
     }
   };
-  
 
   return (
     <>
@@ -109,20 +114,47 @@ export const Restaurants = () => {
         <h1 className="text-3xl md:text-4xl lg:text-6xl font-macondo text-gray-900 dark:text-yellow-500 pb-8">
           Restaurants Management
         </h1>
+        <div>
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full"
+          >
+            +
+          </button>
+
+          {isModalOpen && (
+            <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 backdrop-blur">
+              <div className="bg-white rounded-lg p-8 w-11/12 max-w-md shadow-lg">
+                <button
+                  onClick={() => setIsModalOpen(false)}
+                  className="absolute top-2 right-2"
+                >
+                  X
+                </button>
+                {/* Your form component goes here */}
+                { restaurantCreation === 1 ?
+                  <CreateUserForm /> 
+                  : <CreateRestaurantForm />}
+              </div>
+            </div>
+          )}
+        </div>
         {loading ? (
           <div className="flex justify-center items-center py-8">
             <PropagateLoader color="#622BBC" size={15} />
           </div>
         ) : (
-        tableData.length > 0 &&  (
-          <StatisticsTable
-            head={tableHeader}
-            data={tableData}
-            showActions={true}
-            onApprove={handleAcceptRestaurant}
-            onDelete={() => { console.log("Delete clicked") }}
-          />
-        )
+          tableData.length > 0 && (
+            <StatisticsTable
+              head={tableHeader}
+              data={tableData}
+              showActions={true}
+              onApprove={handleAcceptRestaurant}
+              onDelete={() => {
+                console.log("Delete clicked");
+              }}
+            />
+          )
         )}
       </div>
     </>
