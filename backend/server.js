341580f806a23860bb5a254791aaf51af39c829c
zaiidmo <zaiidmoumnii@gmail.com>
2026-05-11 11:19:23 +0100

    fix: added dummy build script

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ connectDB();
 
 // Configure CORS
 const corsOptions = {
-    origin: "http://localhost:5173", 
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
 };
